c50c5f8358684f66cb4420c814fe30fe16cfc99d
Mathew Paret <mathewparet@gmail.com>
2025-03-04 19:02:50 +1100

    Fixed bug in route definition

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('dashboard', function () {
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
-Route::resource('customers', [CustomerController::class])->except(['edit', 'create', 'show']);
+Route::resource('customers', CustomerController::class)->except(['edit', 'create', 'show']);
