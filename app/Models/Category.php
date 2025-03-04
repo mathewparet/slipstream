3d555741e6d5b8d005632521c52444e1fb73dede
Mathew Paret <mathewparet@gmail.com>
2025-03-04 18:45:41 +1100

    Added customer relationship to category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @var \Illuminate\Database\Eloquent\Relations\HasMany $customers
+ */
 class Category extends Model
 {
     protected $fillable = ['name'];
+
+    public function customers(): HasMany
+    {
+        return $this->hasMany(Customer::class)->chaperone();
+    }
 }
