42d91dde8a9c135e512d18edee0d4d1505a092f1
Mathew Paret <mathewparet@gmail.com>
2025-03-04 20:08:57 +1100

    Make sure customer reference is unique - wrote test cases, updated rules in StoreCustomerRequest and UpdateCustomerRequest

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -57,4 +57,88 @@ class CustomerTest extends TestCase
 
         $response->assertDontSee('Acme Ltd');
     }
+    
+    public function test_customer_reference_must_be_unique(): void
+    {
+        /**
+         * Create first customer
+         */
+        $response = $this->post('/customers', [
+            'name' => 'Acme Ltd',
+            'reference' => 'CUST-01',
+            'start_date' => now()->format('Y-m-d'),
+            'description' => 'Some random description',
+            'category_id' => Category::whereName('Gold')->first()->id
+        ]);
+
+        $response = $this->get('/customers');
+
+        $response->assertSee('Acme Ltd');
+        
+        /**
+         * Attempt to create a second customer with same reference - must be rejected
+         */
+        $response = $this->post('/customers', [
+            'name' => 'Another Company Ltd',
+            'reference' => 'CUST-01',
+            'start_date' => now()->format('Y-m-d'),
+            'description' => null,
+            'category_id' => Category::whereName('Gold')->first()->id
+        ]);
+
+        $response->assertInvalid('reference');
+
+        $response = $this->get('/customers');
+
+        $response->assertDontSee('Another Company Ltd');
+
+        /**
+         * Now create a seconds customer with a different reference and attempt to 
+         * update the second customer's reference to an existing customer reference - 
+         * must be rejected.
+         * 
+         * This is to confirm that the uniqueness of reference is kept while updating as well
+         */
+        $response = $this->post('/customers', [
+            'name' => 'Another Company Ltd',
+            'reference' => 'CUST-02',
+            'start_date' => now()->format('Y-m-d'),
+            'description' => null,
+            'category_id' => Category::whereName('Gold')->first()->id
+        ]);
+
+        $response->assertOk();
+
+        $response = $this->get('/customers');
+
+        $response->assertSee('Another Company Ltd');
+
+        $response = $this->patch('/customers/'. Customer::whereName('Another Company Ltd')->first()->id, [
+            'name' => 'Another Company Ltd',
+            'reference' => 'CUST-01',
+            'start_date' => now()->format('Y-m-d'),
+            'description' => null,
+            'category_id' => Category::whereName('Gold')->first()->id
+        ]);
+
+        $response->assertInvalid('reference');
+
+        /**
+         * Next let's confirm that when a customer is updated it's own reference is accepted in the update
+         */
+
+        $response = $this->patch('/customers/'. Customer::whereName('Another Company Ltd')->first()->id, [
+            'name' => 'Another Company Ltd Renamed',
+            'reference' => 'CUST-02',
+            'start_date' => now()->format('Y-m-d'),
+            'description' => null,
+            'category_id' => Category::whereName('Gold')->first()->id
+        ]);
+
+        $response->assertOk();
+
+        $response = $this->get('/customers');
+
+        $response->assertSee('Another Company Ltd Renamed');
+    }
 }
