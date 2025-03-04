62c0b23b4f1ed7b2150515a48b4b9fed126f9111
Mathew Paret <mathewparet@gmail.com>
2025-03-05 10:42:37 +1100

    Defined scopes to help with filtering records

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * @var \Illuminate\Database\Eloquent\Relations\BelongsTo $category
+ * @method \Illuminate\Database\Eloquent\Builder search($query)
+ * @method \Illuminate\Database\Eloquent\Builder forCategory($category_id)
  */
 class Customer extends Model
 {
@@ -32,5 +36,19 @@ class Customer extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function scopeSearch(Builder $query, $filter): Builder
+    {
+        $filter = '%'.strtolower($filter).'%';
+
+        return $query->where('name', 'like', $filter)
+                        ->orWhere('reference', 'like', $filter)
+                        ->orWhere('description', 'like', $filter);
+    }
+
+    public function scopeForCategory(Builder $query, $category_id): Builder
+    {
+        return $query->where('category_id', $category_id);
     }
 }
