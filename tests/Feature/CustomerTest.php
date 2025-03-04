61e80959ef493bdacaee8bceedcac43201eefb35
Mathew Paret <mathewparet@gmail.com>
2025-03-04 20:08:16 +1100

    Test case to make sure customer has a valid category

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature;
 
-use App\Models\Category;
-use Database\Seeders\CategorySeeder;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
+use App\Models\Category;
+use App\Models\Customer;
+use Database\Seeders\CategorySeeder;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CustomerTest extends TestCase
 {
@@ -38,5 +39,22 @@ class CustomerTest extends TestCase
         $response = $this->get('/customers');
 
         $response->assertSee('Acme Ltd');
+    }
+
+    public function test_customer_must_have_a_valid_category(): void
+    {
+        $response = $this->post('/customers', [
+            'name' => 'Acme Ltd',
+            'reference' => 'CUST-01',
+            'start_date' => now()->format('Y-m-d'),
+            'description' => 'Some random description',
+            'category_id' => Category::latest('id')->first()->id + 1
+        ]);
+
+        $response->assertInvalid('category_id');
+
+        $response = $this->get('/customers');
+
+        $response->assertDontSee('Acme Ltd');
     }
 }
