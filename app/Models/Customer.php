56fbb1d374d90137d2894f3158f62327c6f01152
Mathew Paret <mathewparet@gmail.com>
2025-03-04 18:45:27 +1100

    Defined customer model and relationship to category

---
+++ app/Models/Customer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * @var \Illuminate\Database\Eloquent\Relations\BelongsTo $category
+ */
+class Customer extends Model
+{
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category',
+        'reference',
+        'start_date',
+        'description',
+    ];
+
+    protected function casts()
+    {
+        return [
+            'start_date' => 'date'
+        ];
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
