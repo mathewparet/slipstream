5e9d1394008ffb81595ca04505f381a4357d8909
Mathew Paret <mathewparet@gmail.com>
2025-03-04 20:40:37 +1100

    Changes in laravel's default provided layout to remove references to a logged in user. Did this so that I can use the layout without an active user

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -11,13 +11,12 @@ import { Head, Link } from '@inertiajs/vue3';
         <header class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
             <nav class="flex items-center justify-end gap-4">
                 <Link
-                    v-if="$page.props.auth.user"
-                    :href="route('dashboard')"
+                    :href="route('customers.index')"
                     class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                 >
-                    Dashboard
+                    Customers
                 </Link>
-                <template v-else>
+                <!-- <template v-else>
                     <Link
                         :href="route('login')"
                         class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
@@ -30,7 +29,7 @@ import { Head, Link } from '@inertiajs/vue3';
                     >
                         Register
                     </Link>
-                </template>
+                </template> -->
             </nav>
         </header>
         <div class="duration-750 starting:opacity-0 flex w-full items-center justify-center opacity-100 transition-opacity lg:grow">
